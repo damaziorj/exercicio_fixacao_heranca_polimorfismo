fa604f0dde50da305c2473b5147c31a31cc54722
Sergio Damazio <sergiodamazio@outlook.com>
2020-04-17 03:08:47 -0300

    Finalizado as classes: Product, ImportedProduct e UsedProduct

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,24 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	Locale.setDefault(Locale.US);	
+	Scanner sc = new Scanner(System.in);
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
+	
+	
+	
+	
+	
+	
+	
+	sc.close();
 
 	}
 
